c00450db2ac103cef8a586bd43b0187cdd88f995
Michael van Genderen <mikevg466@gmail.com>
2017-06-17 19:35:55 -0400

    feature: Adds associations for User and Pet to allow for reject and interest storage

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,11 @@ const db = require('../db');
 const User = require('./user');
 const Pet = require('./pet');
 
+User.belongsToMany(Pet, { as: 'Interest', through: 'pet_interest' });
+User.belongsToMany(Pet, { as: 'Reject', through: 'pet_reject' })
+Pet.belongsToMany(User, { through: 'pet_interest' });
+Pet.belongsToMany(User, { through: 'pet_reject' })
+
 module.exports = {
 	db,
 	User,
