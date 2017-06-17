5d448123d68d574f12661226eb4de35c660b51f6
Michael van Genderen <mikevg466@gmail.com>
2017-06-17 19:16:18 -0400

    test: Adds tests for user/pet associations

--- test/model/user.spec.js
+++ test/model/user.spec.js
@@ -1,12 +1,83 @@
 const { expect } = require('chai');
 const db = require('../../server/db');
 const User = db.model('user');
+const Pet = db.model('pet');
 
 describe('User model', () => {
 
   beforeEach(() => {
     return db.sync({ force: true });
   });
+
+  describe('Pet associations', () => {
+    let mike, petList;
+    const testList = [
+      {
+        name: 'Max',
+        image: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg',
+        age: 1,
+        breed: 'puff ball',
+        description: 'Cute and fast!'
+      },
+      {
+        name: 'Spike',
+        image: 'https://s-media-cache-ak0.pinimg.com/736x/a2/42/d5/a242d5a7fca86aeda26676c8627e82c0.jpg',
+        age: 8,
+        breed: 'French Bull',
+        description: 'Always happy!'
+      },
+      {
+        name: 'Snowball',
+        image: 'https://s-media-cache-ak0.pinimg.com/736x/13/04/18/130418ded199cdc061e2ce711c9092fd.jpg',
+        age: 4,
+        breed: 'Persian Fluff',
+        description: 'Loves blowdryers!'
+      }
+    ]
+    beforeEach('load Pets and a User', () => {
+      return Promise.all([
+        User.create({
+          name: 'Mike',
+          email: 'mike.com',
+          password: 'test'
+        }),
+        Pet.create(testList[0]),
+        Pet.create(testList[1]),
+        Pet.create(testList[2]),
+      ])
+        .then(([user, petOne, petTwo, petThree]) => {
+          mike = user;
+          petList = [petOne, petTwo, petThree];
+        });
+    });
+
+    it('User can associate to pets they\'re interested in', () => {
+      return Promise.all([
+        mike.addInterest(petList[0]),
+        mike.addInterest(petList[2])
+      ])
+        .then(() => mike.getInterests())
+        .then(interestList => {
+          expect(interestList).to.be.an('array');
+          expect(interestList).to.have.a.lengthOf(2);
+          expect(interestList[0].name).to.equal(petList[0].name);
+          expect(interestList[1].name).to.equal(petList[2].name);
+        });
+    });
+    it('User can associate to pets they\'re not interested in', () => {
+      return Promise.all([
+        mike.addReject(petList[1]),
+        mike.addReject(petList[2])
+      ])
+        .then(() => mike.getRejects())
+        .then(rejectList => {
+          expect(rejectList).to.be.an('array');
+          expect(rejectList).to.have.a.lengthOf(2);
+          expect(rejectList[0].name).to.equal(petList[1].name);
+          expect(rejectList[1].name).to.equal(petList[2].name);
+        });
+    });
+  }); // end describe('Pet associations');
 
   describe('instanceMethods', () => {
 
