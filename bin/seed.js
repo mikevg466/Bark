d75c108ec485de58ba0ebba7df2235c2d235b805
Michael van Genderen <mikevg466@gmail.com>
2017-06-19 01:15:11 -0400

    feature: Adds more seed data

--- bin/seed.js
+++ bin/seed.js
@@ -38,6 +38,41 @@ const data = {
       age: 15,
       breed: 'Grupy Persian',
       description: 'Always grumpy. A real downer...'
+    },
+    {
+      name: 'Repunzel'
+      image: 'https://s-media-cache-ak0.pinimg.com/originals/8c/c7/d6/8cc7d69bc563f2b60fb202237e99ba30.jpg',
+      age: 10,
+      breed: 'Crazy Long Hair',
+      description: 'Super long hair.  Will take you forever to comb. Repunzel, repunzel...'
+    },
+    {
+      name: 'Rex',
+      image: 'http://buzz.iloveindia.com/wp-content/uploads/2015/09/large-dog-breeds-10.2.jpg',
+      age: 8,
+      breed: 'Bear?',
+      description: 'Is it a bear or a dog?!?!'
+    },
+    {
+      name: 'Zim',
+      image: 'https://featuredcreature.com/wp-content/uploads/2011/12/DSC_0156.jpg',
+      age: 4,
+      breed: 'Alien',
+      description: 'Oh god... I think this is an alien...'
+    },
+    {
+      name: 'Spot',
+      image: 'https://s-media-cache-ak0.pinimg.com/736x/25/67/48/256748946d8f71498fbdd6827bf186a3.jpg',
+      age: 1,
+      breed: 'Munchkin',
+      description: 'So cute I\'m gonna die!!! Sooooo fluffyyyy!!!'
+    },
+    {
+      name: 'Grrr',
+      image: 'https://s-media-cache-ak0.pinimg.com/736x/29/fa/14/29fa1441f2a0446e9aa45cee74495a83.jpg',
+      age: 1,
+      breed: 'Toy',
+      description: 'This dog is too cute to be real. Just adorable.'
     }
   ],
   user: [
@@ -63,11 +98,13 @@ db.sync({force: true})
 .then(() => Promise.all([
   db.model('user').findOne({where: {email: 'owner@test.com'}}),
   db.model('pet').findOne({where: {name: 'Snowball'}}),
-  db.model('pet').findOne({where: {name: 'Twitch'}})
+  db.model('pet').findOne({where: {name: 'Spot'}}),
+  db.model('pet').findOne({where: {name: 'Grrr'}})
 ]))
-.then(([user, petOne, petTwo]) => Promise.all([
+.then(([user, petOne, petTwo, petThree]) => Promise.all([
   user.addAdopt(petOne),
-  user.addAdopt(petTwo)
+  user.addAdopt(petTwo),
+  user.addAdopt(petThree)
 ]))
 .then(() => {
   console.log("Finished inserting seed data");
